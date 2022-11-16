4dd5d6c9315d7856a77b1dd1baf9e70ba0d61eda
mesheni <bunin.ivan14@yandex.ru>
2022-11-16 23:50:27 +0300

    Удалил из проекта WinForm реализацию программы.

--- VidDownload.WPF/AssemblyInfo.cs
+++ VidDownload.WPF/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -9,3 +10,22 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+[assembly: AssemblyTitle("VidDownload.WPF")]
+[assembly: AssemblyDescription("Программа для загрузки видео с разных платформ на основе yt-dlp.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ivan Bunin")]
+[assembly: AssemblyProduct("VidDownload.WPF")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("854de903-371e-4360-b450-3c1a0c710356")]
+
+[assembly: AssemblyVersion("0.5.1.0")]
+[assembly: AssemblyFileVersion("0.5.1.0")]
