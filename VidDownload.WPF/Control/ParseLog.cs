01dd4c0e0ff66d1ff778492d2292637be05e32be
mesh <mesh@DESKTOP-ISAVJA7>
2026-05-19 00:57:54 +0300

    Fix ParseLog to use CultureInfo.InvariantCulture

--- VidDownload.WPF/Control/ParseLog.cs
+++ VidDownload.WPF/Control/ParseLog.cs
@@ -1,33 +1,19 @@
-﻿using System.Text.RegularExpressions;
-
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace VidDownload.WPF.Control
 {
-
-    /// <summary>
-    /// Анализирует строку лога для извлечения процентного значения.
-    /// Ищет шаблон, подобный "12.3", и преобразует его в double. 
-    /// Возвращает 0, если совпадение не найдено.
-    /// </summary>
-    internal class ParseLog // Класс для парсинга лога yt-dlp
+    internal class ParseLog
     {
         public static double Parse(string log)
         {
-            string buff;
-            double stringResult = 0;
+            var match = Regex.Match(log, @"\d{1,2}\.\d");
+            if (match.Success)
+            {
+                return double.Parse(match.Value, CultureInfo.InvariantCulture);
+            }
 
-            // Парсинг лога
-            if (Regex.IsMatch(log, @"(\d{1}|\d{2})\.\d{1}"))
-            {
-                buff = Regex.Match(log, @"(\d{1}|\d{2})\.\d{1}").Value.ToString();
-                buff = buff.Replace('.', ',');
-                stringResult = Convert.ToDouble(buff);
-                return stringResult;
-            }
-            else
-            {
-                return 0;
-            }
+            return 0;
         }
     }
 }
